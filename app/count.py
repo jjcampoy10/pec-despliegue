07504aee32e19d346679485ba509b39178c97cc2
jjcampoy10 <josecaampoy10@gmail.com>
2025-12-04 20:05:52 +0100

    Corregir count.py para que solo cuente del 1 al 10 sin bucle infinito

--- app/count.py
+++ app/count.py
@@ -1,14 +1,3 @@
-import time
-
-def main():
-    for i in range(1, 11):
-        print(i, flush=True)
-        time.sleep(0.3)
-
-    print("Script terminado. Manteniendo el contenedor vivo...")
-    while True:
-        time.sleep(60)  # Mantiene el contenedor activo
-
-if __name__ == "__main__":
-    main()
+for i in range(1, 11):
+    print(i)
 
