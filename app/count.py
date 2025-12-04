9e1e8d0f79e057bd8e2beffbe14361ca01c52a7a
jjcampoy10 <josecaampoy10@gmail.com>
2025-12-04 19:58:53 +0100

    Mantener el contenedor vivo tras ejecutar el script

--- app/count.py
+++ app/count.py
@@ -1,3 +1,14 @@
-for i in range(1, 11):
-    print(i)
+import time
+
+def main():
+    for i in range(1, 11):
+        print(i, flush=True)
+        time.sleep(0.3)
+
+    print("Script terminado. Manteniendo el contenedor vivo...")
+    while True:
+        time.sleep(60)  # Mantiene el contenedor activo
+
+if __name__ == "__main__":
+    main()
 
